ead9a812698595a7f3febff0874427ac1ddc9c14
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2025-03-22 21:20:51 +0530

    init Raffle/lottery contract WIP
    Signed-off-by: John Titor <50095635+JohnRTitor@users.noreply.github.com>

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -1,34 +1,73 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.28;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+/**
+ * @title Raffle Contract
+ * @author Masum Reza, originally by Patrick Collins in FCC course
+ * @notice This contract implements a decentralized lottery system
+ * @dev This contract uses Chainlink Oracle for verifiably random number generation
+ * and Chainlink Keepers for automated execution
+ */
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
+/**
+ * @notice Error thrown when a user tries to enter the raffle with insufficient ETH
+ */
+error Raffle__NotEnoughETHEntered();
 
-contract Lock {
-    uint public unlockTime;
-    address payable public owner;
+/**
+ * @title Raffle Contract
+ * @notice Implements a decentralized lottery where users can enter by paying an entrance fee
+ * and a winner is selected randomly at specified intervals
+ */
+contract Raffle {
+    /* State variables */
+    uint256 private immutable i_entraceFee;
+    address payable[] private s_players;
 
-    event Withdrawal(uint amount, uint when);
-
-    constructor(uint _unlockTime) payable {
-        require(
-            block.timestamp < _unlockTime,
-            "Unlock time should be in the future"
-        );
-
-        unlockTime = _unlockTime;
-        owner = payable(msg.sender);
+    /**
+     * @notice Constructor sets the entrance fee for the raffle
+     * @param entranceFee The amount of ETH required to enter the raffle
+     */
+    constructor(uint256 entranceFee) {
+        i_entraceFee = entranceFee;
     }
 
-    function withdraw() public {
-        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
+    /**
+     * @notice Function for users to enter the raffle
+     * @dev Users must send at least the entrance fee to participate
+     * Reverts if insufficient ETH is sent
+     */
+    function enterRaffle() public payable {
+        if (msg.value < i_entraceFee) {
+            revert Raffle__NotEnoughETHEntered();
+        }
+        s_players.push(payable(msg.sender));
+    }
 
-        require(block.timestamp >= unlockTime, "You can't withdraw yet");
-        require(msg.sender == owner, "You aren't the owner");
+    /**
+     * @notice Function to select a random winner from the participants
+     * @dev This function is not implemented yet
+     * Will use Chainlink VRF for verifiable randomness
+     */
+    /*
+    function pickRandomWinner() {
 
-        emit Withdrawal(address(this).balance, block.timestamp);
+    }
+    */
 
-        owner.transfer(address(this).balance);
+    /**
+     * @notice Returns the entrance fee for the raffle
+     * @return uint256 The entrance fee in wei
+     */
+    function getEntranceFee() public view returns (uint256) {
+        return i_entraceFee;
+    }
+
+    /**
+     * @notice Returns the address of a player at the specified index
+     * @param index The position in the players array
+     * @return address The player's address
+     */
+    function getPlayer(uint256 index) public view returns (address) {
+        return s_players[index];
     }
 }
