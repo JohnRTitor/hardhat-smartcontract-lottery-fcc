4560b3a0cc608cc95ae4f452a6c07a4abe9a5b6f
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2025-03-25 12:56:58 +0530

    contracts/Raffle: init upkeep integration to auto run
    KeeperCompatibleInterface imported from chainlink/contracts node package, go to
    https://www.npmjs.com/package/@chainlink/contracts?activeTab=code to verify the import location
    
    It has two functions, checkUpkeep, and performUpkeep. checkUpkeep checks if we need to run this
    contract again, if true, it runs performUpkeep. These functions run off chain on an Oracle, since
    a contract can't run itself.
    
    Add a enum state variable for the lottery. During critical section/CALCULATING PHASE,
    we do not allow anyone to enter the raffle. After a winner is decided we set it to OPEN
    again.
    
    Tests currently broken, implementation incomplete.
    
    Signed-off-by: John Titor <50095635+JohnRTitor@users.noreply.github.com>

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -3,27 +3,37 @@
  * @author Masum Reza, originally by Patrick Collins in FCC course
  * @notice This contract implements a decentralized lottery system
  * @dev This contract uses Chainlink Oracle for verifiably random number generation
- * and Chainlink Keepers for automated execution
+ * and Chainlink Keepers/Automation for automated execution
  */
 
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
+// imports for Chainlink VRF - to request randomness
 import "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
 import "@chainlink/contracts/src/v0.8/vrf/interfaces/VRFCoordinatorV2Interface.sol";
+// imports for Chainlink Automation - to automate execution at intervals
+import "@chainlink/contracts/src/v0.8/automation/interfaces/KeeperCompatibleInterface.sol";
 
 /**
  * @notice Error thrown when a user tries to enter the raffle with insufficient ETH
  */
 error Raffle__NotEnoughETHEntered();
 error Raffle__TransferFailed();
+error Raffle__NotOpen();
 
 /**
  * @title Raffle Contract
  * @notice Implements a decentralized lottery where users can enter by paying an entrance fee
  * and a winner is selected randomly at specified intervals
  */
-contract Raffle is VRFConsumerBaseV2 {
+contract Raffle is VRFConsumerBaseV2, KeeperCompatibleInterface {
+    /* Type/Enum declarations */
+    enum RaffleState {
+        OPEN,
+        CALCULATING
+    } // 0 = OPEN, 1 = CALCULATING
+
     /* State variables */
     uint256 private immutable i_entraceFee;
     address payable[] private s_players;
@@ -58,6 +68,9 @@ contract Raffle is VRFConsumerBaseV2 {
 
     // Lottery variables
     address private s_recentWinner;
+    RaffleState private s_raffleState;
+    uint256 private s_lastTimeStamp;
+    uint256 private immutable i_interval;
 
     /* Events */
     // events can't be accessed by smart contracts
@@ -76,13 +89,19 @@ contract Raffle is VRFConsumerBaseV2 {
         uint256 entranceFee,
         bytes32 gasLane,
         uint64 subscriptionId,
-        uint32 callbackGasLimit
+        uint32 callbackGasLimit,
+        uint256 interval
     ) VRFConsumerBaseV2(vrfCoordinatorAddress) {
         i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorAddress);
         i_entraceFee = entranceFee;
         i_gasLane = gasLane;
         i_subscriptionId = subscriptionId;
         i_callbackGasLimit = callbackGasLimit;
+        // Open the lottery
+        s_raffleState = RaffleState.OPEN;
+        // set it to the current timestamp
+        s_lastTimeStamp = block.timestamp;
+        i_interval = interval;
     }
 
     /**
@@ -91,11 +110,38 @@ contract Raffle is VRFConsumerBaseV2 {
      * Reverts if insufficient ETH is sent
      */
     function enterRaffle() public payable {
+        if (s_raffleState != RaffleState.OPEN) {
+            revert Raffle__NotOpen();
+        }
         if (msg.value < i_entraceFee) {
             revert Raffle__NotEnoughETHEntered();
         }
         s_players.push(payable(msg.sender));
         emit RaffleEnter(msg.sender);
+    }
+
+    /**
+     * @dev This is the function that the Chainlink automation nodes call
+     * they look for the `upkeepNeeded` to return true
+     * if it returns true, the Chainlink automation nodes will call performUpkeep
+     * The following should be true in order to return true:
+     * 1. The raffle is open
+     * 2. The upkeep interval has passed
+     * 3. The contract has enough ETH to pay the prize and have at least one player
+     * 4. The contract has a valid subscription ID and funded with LINK
+     */
+    function checkUpkeep(
+        bytes calldata /* checkData */
+    )
+        external
+        view
+        override
+        returns (bool upkeepNeeded, bytes memory /* performData */)
+    {
+        bool isOpen = s_raffleState == RaffleState.OPEN;
+        bool hasTimePassed = (block.timestamp - s_lastTimeStamp) > i_interval;
+        bool hasPlayers = s_players.length >= 1;
+        bool hasBalance = address(this).balance > 0;
     }
 
     /**
@@ -107,6 +153,9 @@ contract Raffle is VRFConsumerBaseV2 {
         // Request the random number
         // Once we get it, do something with it
         // 2 transaction process
+
+        // Lock the lottery
+        s_raffleState = RaffleState.CALCULATING;
 
         uint256 requestId = i_vrfCoordinator.requestRandomWords(
             i_gasLane,
@@ -134,10 +183,16 @@ contract Raffle is VRFConsumerBaseV2 {
         address payable recentWinner = s_players[indexOfWinner];
         s_recentWinner = recentWinner;
 
+        // Send winner the prize money
         (bool success, ) = recentWinner.call{value: address(this).balance}("");
         if (!success) {
             revert Raffle__TransferFailed();
         }
+
+        // reset players array
+        s_players = new address payable[](0);
+        // Reopen the lottery
+        s_raffleState = RaffleState.OPEN;
 
         emit WinnerPicked(recentWinner);
     }
