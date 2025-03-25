d03247d8742ef3ada90d38d77685d21e09c89741
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2025-03-25 21:50:55 +0530

    contracts: add VRFCoordinatorV2Mock.sol
    by importing it from the node package @chainlink/contracts
    
    Signed-off-by: John Titor <50095635+JohnRTitor@users.noreply.github.com>

---
+++ contracts/Mocks/VRFCoordinatorV2Mock.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+// the best way to find this, is
+// not go to the github repo, as that may be too updated
+// instead go to https://www.npmjs.com/package/@chainlink/contracts?activeTab=code
+// browse code for your exact version
+import "@chainlink/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2Mock.sol";
